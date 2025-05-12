55be6f56724061dd617aa4d84d9c125ada28d1d6
Edward Nolan <eddiejnolan@gmail.com>
2025-05-12 02:59:25 -0400

    wip

--- beman_module/beman_module.py
+++ beman_module/beman_module.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 import argparse
 import configparser
 import filecmp
--- beman_module/test_beman_module.py
+++ beman_module/test_beman_module.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 import beman_module
 import os
 import pathlib
